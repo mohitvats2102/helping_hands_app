6ef94369bec319c20219acdd5afc175b5d86418c
mohitvats2102 <mohitvatsktl@gmail.com>
2021-01-19 23:19:43 +0530

    Added Container

--- lib/new.dart
+++ lib/new.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class MyClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container();
   }
 }
