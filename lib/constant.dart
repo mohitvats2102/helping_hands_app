6a4362eb3aee9e1e3974d08635e61748f6816e72
mohitvats2102 <mohitvatsktl@gmail.com>
2021-01-21 01:27:36 +0530

    Start Building

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 const TextStyle kloginText = TextStyle(
   color: Colors.white,
-  fontSize: 40,
+  fontSize: 45,
   fontWeight: FontWeight.w400,
+);
+
+const BoxDecoration kloginContainerDecoration = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(38),
+  ),
 );
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,28 +7,34 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF006BFF),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Helping',
-                style: kloginText,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 18, top: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Helping',
+                    style: kloginText,
+                  ),
+                  Text(
+                    'Hands',
+                    style: kloginText.copyWith(
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'Hands',
-                style: kloginText.copyWith(
-                  fontWeight: FontWeight.w900,
-                ),
+            ),
+            SizedBox(height: 60),
+            Expanded(
+              child: Container(
+                decoration: kloginContainerDecoration,
               ),
-              Form(
-                child: Column(
-                  children: [],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
