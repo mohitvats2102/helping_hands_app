866267336cf63482a99fc8f91562939b1f98625e
mohit_vats <46342546+mohitvats2102@users.noreply.github.com>
2021-01-22 22:19:17 +0530

    Update category_screen.dart

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -2,71 +2,65 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:helping_hands_app/Widget/GridViewFileFrame.dart';
 import 'package:helping_hands_app/demo_examples.dart';
-import 'package:helping_hands_app/module/GridViewFile.dart';
-import 'package:helping_hands_app/screens/login_screen.dart';
-class  CategoryScreen extends StatelessWidget {
-   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class CategoryScreen extends StatefulWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   static String categoryScreen = '/categoryScreen';
-  final  Function changetheme;
- CategoryScreen(this.changetheme);
-void logout(BuildContext ctx)
-  {
-    _auth.signOut();
-    Navigator.of(ctx).pushReplacementNamed(LoginScreen.loginScreen);
+  final Function changetheme;
+  CategoryScreen(this.changetheme);
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
 
+class _CategoryScreenState extends State<CategoryScreen> {
+  void logout() {
+    FirebaseAuth.instance.signOut();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Ctaegories"),
-        
-           actions: [   
-                     
-                     DropdownButtonHideUnderline(
-                      child: DropdownButton(
-              
-                      icon: Icon(Icons.more_vert), 
-                      items: [
-                        
-                          DropdownMenuItem(
-                          value: "Theme",
-                          child: Text("Theme")),
-                          DropdownMenuItem(value:"Logout",child: Text("Logout"))
-                              ],
-                      onChanged: (itemIdentifier)
-                      {
-                         if(itemIdentifier=='Theme')
-                         {
-                              changetheme();
-                         }
-                         if(itemIdentifier=='Logout')
-                         {
-                               logout(context);
-                         }
-                      },),)],
-      
+        title: Text("Ctaegories"),
+        actions: [
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(Icons.more_vert),
+              items: [
+                DropdownMenuItem(value: "Theme", child: Text("Theme")),
+                DropdownMenuItem(value: "Logout", child: Text("Logout"))
+              ],
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'Theme') {
+                  widget.changetheme();
+                }
+                if (itemIdentifier == 'Logout') {
+                  logout();
+                }
+              },
+            ),
+          )
+        ],
       ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(vertical:20.0,horizontal:10.0),
-            child: GridView(
-        children:Demo_Example
-              .map(
-                (catData) => GridViewFileFrame(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+        child: GridView(
+          children: Demo_Example.map(
+            (catData) => GridViewFileFrame(
               catData.id,
               catData.title,
               catData.color,
             ),
-        )
-              .toList(),
-        gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent
-        (maxCrossAxisExtent: 200,childAspectRatio: 2/2,
-         crossAxisSpacing: 20,
-        mainAxisSpacing: 50,) ,
-
-      ),
+          ).toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 2 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 50,
           ),
+        ),
+      ),
     );
   }
 }
