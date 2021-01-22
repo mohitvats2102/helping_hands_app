4d779ba91f07f446cd45f29e20ad5171a531e63e
mohitvats2102 <mohitvatsktl@gmail.com>
2021-01-22 22:16:17 +0530

    Refined Code

--- lib/widget/GridViewFileFrame.dart
+++ lib/widget/GridViewFileFrame.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-class  GridViewFileFrame extends StatelessWidget {
+
+class GridViewFileFrame extends StatelessWidget {
   final String id;
   final String title;
   final Color color;
 
   GridViewFileFrame(this.id, this.title, this.color);
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +14,14 @@ class  GridViewFileFrame extends StatelessWidget {
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
-        padding: const EdgeInsets.fromLTRB(35.0, 30.0, 35.0,80.0 ),
+        padding: const EdgeInsets.fromLTRB(35.0, 30.0, 35.0, 80.0),
         child: Text(
           title,
-          
           style: Theme.of(context).textTheme.title,
-
         ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-           colors: [
+            colors: [
               color.withOpacity(0.7),
               color,
             ],
